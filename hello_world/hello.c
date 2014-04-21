24baa816c01fc96e608ae0e33df336faf3345b4b
khrogos <khrogos@debian>
2014-04-21 21:12:17 +0200

    write on debug log level

--- hello_world/hello.c
+++ hello_world/hello.c
@@ -3,11 +3,11 @@
 
 int 
 init_module(void){
-	printk(KERN_INFO "Hello, World\n");
+	printk(KERN_DEBUG "Hello, World\n");
 	return 0;
 }
 
 void 
 cleanup_module(void){
-	printK(KERN_INFO "good bye, world\n");
+	printK(KERN_DEBUG "good bye, world\n");
 }
