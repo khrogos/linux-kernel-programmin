1f45f3d4682146dbeebfb999e8103e6299b6759b
khrogos <khrogos@debian>
2014-04-21 20:56:36 +0200

    write the asic hello

--- hello_world/hello.c
+++ hello_world/hello.c
@@ -1,1 +1,13 @@
+#include <linux/module.h>
 #include <linux/kernel.h>
+
+int 
+init_module(void){
+	printk(KERN_INFO "Hello, World\n");
+	return 0;
+}
+
+void 
+cleanup_module(void){
+	printK(KERN_INFO "good bye, world\n");
+}
