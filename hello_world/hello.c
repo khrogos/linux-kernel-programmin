aa7507fb3cdac0541dab794f693c216ace873350
khrogos <khrogos@debian>
2014-04-22 23:37:39 +0200

    know use _init and __exit macros

--- hello_world/hello.c
+++ hello_world/hello.c
@@ -1,13 +1,17 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 
-int 
-init_module(void){
+static int 
+__init launch(void){
 	printk(KERN_DEBUG "Hello, World\n");
 	return 0;
 }
 
-void 
-cleanup_module(void){
+static void 
+__exit destroy(void){
 	printk(KERN_DEBUG "good bye, world\n");
 }
+
+module_init(launch);
+module_exit(destroy);
